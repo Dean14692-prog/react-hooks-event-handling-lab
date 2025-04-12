3af184dd9612753441fc0d9b2e57145c1afe802d
Dean14692-prog <dennismuasyangui@gmail.com>
2025-04-12 21:02:11 +0300

    Event

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
-// Code EyesOnMe Component Here
+// components/EyesOnMe.js
+import React from "react";
+
+function EyesOnMe() {
+  // Basic event handlers for focus and blur events
+  const handleFocus = () => {
+    console.log("Good!");
+  };
+
+  const handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  return (
+    <button
+      onFocus={handleFocus} // Event listener for focus
+      onBlur={handleBlur} // Event listener for blur
+    >
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
-// Code Keypad Component Here
+// components/Keypad.js
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  // Basic event handler for the change event
+  const handleChange = () => {
+    console.log("Entering password...");
+  };
+
+  return (
+    <input
+      type="password"
+      onChange={handleChange} // Event listener for input change
+    />
+  );
 }
 
 export default Keypad;
